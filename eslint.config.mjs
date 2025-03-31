8620bf00f3bb2c0c7393795fee5648a4df69ba22
luckytzu <tatindylan@gmail.com>
2025-03-31 10:03:47 +0200

    Fix ESLint configuration and Prettier formatting issues
    - Updated ESLint config to comply with flat config format
    - Defined global variables like 'module', 'require', 'process', and 'console'
    - Fixed Prettier formatting issues related to unnecessary spaces

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,26 @@ import pluginJs from "@eslint/js";
 import prettier from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 import jsdoc from "eslint-plugin-jsdoc";
+
 export default [
   pluginJs.configs.recommended,
   prettierConfig,
   {
     plugins: {
-      prettier,
-      jsdoc,
+      prettier: prettier,
+      jsdoc: jsdoc,
+    },
+    languageOptions: {
+      globals: {
+        module: "readonly", // Déclare module comme global
+        require: "readonly", // Déclare require comme global
+        process: "readonly", // Déclare process comme global
+        console: "readonly", // Déclare console comme global
+      },
     },
     rules: {
       "no-unused-vars": "warn",
-      "no-undef": "warn",
+      "no-undef": "warn", // Les variables globales définies par Node.js ne poseront plus de problèmes ici
       camelcase: "error",
       "prettier/prettier": "error",
       "jsdoc/check-tag-names": "error",
