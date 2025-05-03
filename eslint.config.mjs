2754e3f4347bc44dedec0eabbd3070b5d467bd7d
luckytzu <tatindylan@gmail.com>
2025-05-03 10:14:37 +0200

    feat(prisma): set up data retrieval and add JSON output file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import pluginJs from "@eslint/js";
 import prettier from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 import jsdoc from "eslint-plugin-jsdoc";
+
 export default [
   pluginJs.configs.recommended,
   prettierConfig,
@@ -9,6 +10,17 @@ export default [
     plugins: {
       prettier,
       jsdoc,
+    },
+    languageOptions: {
+      globals: {
+        Iterator: "readonly",
+        module: "readonly",
+        require: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        exports: "readonly",
+        console: "readonly",
+      },
     },
     rules: {
       "no-unused-vars": "warn",
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     "^.+\\.tsx?$": ["ts-jest", {}],
   },
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "json", "html"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!**/node_modules/**",
+    "!src/**/__tests__/**",
+    "!src/lib/**",
+  ],
+  testPathIgnorePatterns: ["/node_modules/", "/src/lib/"],
 };
