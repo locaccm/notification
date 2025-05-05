321102fdd9e1d0bc3e0704ef88a2deeba76fccfc
luckytzu <tatindylan@gmail.com>
2025-05-05 17:49:29 +0200

    rebase

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,17 +1,8 @@
-/* eslint-env node */
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.tsx?$": ["ts-jest", {}],
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "json"],
-  coverageDirectory: "./coverage",
-  collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
-    "!src/**/*.d.ts",
-    "!src/tests/**/*",
-  ],
-  coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/src/index.ts"],
 };
