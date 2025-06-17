837f9c0831880364d582c4a2c4280765aabc0307
luckytzu <tatindylan@gmail.com>
2025-06-17 11:35:29 +0200

    edit package.json db pull delete

--- src/interfaces/tenant.interface.ts
+++ src/interfaces/tenant.interface.ts
@@ -1,23 +1,23 @@
 // Defines the shape of a lease with relevant dates for start, payment, and end
 export interface Lease {
-  LEAD_START: Date | null;
-  LEAD_END: Date | null;
-  LEAD_PAYMENT: Date | null;
+  LEAD_START: Date;
+  LEAD_END: Date;
+  LEAD_PAYMENT: Date;
 }
 
 // Defines the structure of an event with a title and start/end dates
 export interface Event {
-  EVEC_LIB: string | null;
-  EVED_START: Date | null;
-  EVED_END: Date | null;
+  EVEC_LIB: string;
+  EVED_START: Date;
+  EVED_END: Date;
 }
 
 // Defines the shape of a tenant including personal info and related leases and events
 export interface Tenant {
   USEN_ID: number;
-  USEC_FNAME: string | null;
-  USEC_LNAME: string | null;
-  USEC_MAIL: string | null;
+  USEC_FNAME: string;
+  USEC_LNAME: string;
+  USEC_MAIL: string;
   leases: Lease[];
   events: Event[];
 }
